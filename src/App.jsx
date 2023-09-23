951e83414d2d713a66e0acac7cdbdf3219c3311a
JayVaghani-Dev <jayvaghanidev@gmail.com>
2023-09-23 22:29:17 +0530

    fundamentel

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./app.css";
+import Form1 from "./components/Form1";
+
+function App() {
+  return (
+    <div>
+      <Form1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form1.jsx
@@ -0,0 +1,88 @@
+import { DevTool } from "@hookform/devtools";
+import React from "react";
+import { useForm } from "react-hook-form";
+
+function Form1() {
+  const form = useForm();
+
+  const { register, control, handleSubmit, formState } = form;
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  const { errors } = formState;
+
+  console.log(errors);
+
+  return (
+    <div>
+      <h1>YouTube Form</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          {...register("username", {
+            required: {
+              value: true,
+              message: "Username is required",
+            },
+          })}
+        />
+        <p className="error">{errors.username?.message}</p>
+        <label htmlFor="email">E-mail</label>
+        <input
+          type="email"
+          id="email"
+          {...register("email", {
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: "Invalid Email",
+            },
+            required: {
+              value: true,
+              message: "Email is required",
+            },
+            // validate: (felidValue) => {
+            //   return (
+            //     felidValue !== "admin@gmail.com" || "Enter different email"
+            //   );
+            // },
+
+            validate: {
+              notAdmin: (felidValue) => {
+                return (
+                  felidValue !== "admin@gmail.com" || "Enter different email"
+                );
+              },
+
+              notBlackListed: (felidValue) => {
+                return (
+                  !felidValue.endsWith("blacklisted.com") ||
+                  "This domain is black listed"
+                );
+              },
+            },
+          })}
+        />
+        <p className="error">{errors.email?.message}</p>
+        <label htmlFor="channel">Channel</label>
+        <input
+          type="text"
+          id="channel"
+          {...register("chanel", {
+            required: "Chanel name is required",
+          })}
+        />
+        <p className="error">{errors.chanel?.message}</p>
+
+        <button>Submit</button>
+      </form>
+      <DevTool control={control} />
+    </div>
+  );
+}
+
+export default Form1;
